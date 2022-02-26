6285db5ae4704b28831eae15f7e48dfc9d31ee39
Ismail Mohammedali <67723609+ismail-mohammedali@users.noreply.github.com>
2022-02-26 15:29:43 -0500

    Add files via upload

---
+++ LeetCode/9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        forward = str(x)
+        backward = str(x)[::-1]
+        if forward == backward: 
+            return True
+        else: 
+            return False
+        
